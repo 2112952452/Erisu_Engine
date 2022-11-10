624dce56e460a6fbbb4efabc07f14a847cc9ba1d
Adarion <2112952452@qq.com>
2022-11-10 17:36:21 +0800

    ver 0.6.1

--- Source/Scripts/init.py
+++ Source/Scripts/init.py
@@ -1,7 +1,7 @@
 # 全局变量初始化
 WindowWidth = 1920
 WindowHeight = 1080
-WindowTitle = "Nilou Nilou"
+WindowTitle = "Erisu Engine"
 
 GalTextBoxFontPath = "Resources/font/SourceHanSansSC-Light.otf"
 GalTextBoxFontSize = 30
